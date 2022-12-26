508cb1c8e9eb4906264306d47d1daa74dcf4c91d
Birimumaaso Rogers <birimumaaso14roja@gmail.com>
2022-12-26 11:32:07 +0300

    Admin Dashboard edited and styled

---
+++ moderator/static/moderator/js/base.js
@@ -0,0 +1,21 @@
+// ROOT
+var root = document.querySelector(':root');
+
+// SIDEBAR
+const menuItems = document.querySelectorAll('.menu-item')
+
+// ===================== SIDEBAR ====================//
+
+// remove active class from all menu items
+const changeActiveItem = () => {
+    menuItems.forEach(item => {
+        item.classList.remove('active')
+    })
+}
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        changeActiveItem();
+        // item.classList.add('active');
+    })
+})
